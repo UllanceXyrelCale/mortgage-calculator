8aebd75bf113702ee460cd442ea56182aea94a17
Ullance Xyrel Cale <uxjcale710@gmail.com>
2025-07-22 09:49:15 -0400

    added safety

--- src/com/ullance/Main.java
+++ src/com/ullance/Main.java
@@ -12,16 +12,37 @@ public class Main {
 
         // Input variables
         Scanner scanner = new Scanner(System.in);
+        double principal = 0;
+        float monthlyInterestRate = 0;
+        int months = 0;
 
-        System.out.print("Principal: ");
-        double principal = scanner.nextDouble();
+        // Grabbing input
+        do {
+            System.out.print("Principal ($1K - $1M): ");
+            principal = scanner.nextDouble();
+            if (principal < 1_000 || principal > 1_000_000){
+                System.out.println("Enter a number between 1,000 and 1,000,000.");
+            }
+        } while (principal < 1_000 || principal > 1_000_000);
 
-        System.out.print("Annual Interest Rate: ");
-        float monthlyInterestRate = (scanner.nextFloat()/PERCENT)/ANNUAL_MONTHS; // annual interest rate turned into monthly
+        do {
+            System.out.print("Annual Interest Rate: ");
+            monthlyInterestRate = (scanner.nextFloat());
+            if (monthlyInterestRate <= 0 || monthlyInterestRate > 30){
+                System.out.println("Enter a value greater than 0 and less than or equal 30.");
+            }
+        } while (monthlyInterestRate <= 0 || monthlyInterestRate > 30);
+        monthlyInterestRate = (monthlyInterestRate /PERCENT)/ANNUAL_MONTHS; // annual interest rate turned into monthly
 
-        System.out.print("Years: ");
-        int months = scanner.nextInt() * ANNUAL_MONTHS; // years turned into months
-
+        do {
+            System.out.print("Period (Years): ");
+            months = scanner.nextInt();
+            if (months < 1 || months > 30){
+                System.out.println("Enter a value between 1 and 30.");
+            }
+        } while (months < 1 || months > 30);
+        months = months  * ANNUAL_MONTHS; // years turned into months
+        
         // Math hehe
         double mortgage = principal * ((monthlyInterestRate * Math.pow(1 + monthlyInterestRate, months))/(Math.pow(1 + monthlyInterestRate, months) - 1));
         String currencyMortgage = NumberFormat.getCurrencyInstance().format(mortgage);
